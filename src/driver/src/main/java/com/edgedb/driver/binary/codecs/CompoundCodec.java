e3ef2d652a2fe0e31b57c0e810e54f18ddbccec6
Quin Lynch <lynchquin@gmail.com>
2023-07-28 16:19:02 -0300

    implement CompoundCodec

--- src/driver/src/main/java/com/edgedb/driver/binary/codecs/CompoundCodec.java
+++ src/driver/src/main/java/com/edgedb/driver/binary/codecs/CompoundCodec.java
@@ -8,11 +8,14 @@ import com.edgedb.driver.exceptions.EdgeDBException;
 import org.jetbrains.annotations.Nullable;
 
 import javax.naming.OperationNotSupportedException;
+import java.util.Collection;
 import java.util.UUID;
+
+import static com.edgedb.driver.util.BinaryProtocolUtils.INT_SIZE;
 
 public class CompoundCodec extends CodecBase<Object> {
     private final TypeOperation operation;
-    private final Codec<?>[] elements;
+    private final Codec<?>[] innerCodecs;
 
     public CompoundCodec(
             UUID id,
@@ -23,17 +26,69 @@ public class CompoundCodec extends CodecBase<Object> {
         super(id, metadata, Object.class);
 
         this.operation = operation;
-        this.elements = elements;
+        this.innerCodecs = elements;
     }
 
+    public TypeOperation getTypeOperation() {
+        return this.operation;
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
     @Override
     public void serialize(PacketWriter writer, @Nullable Object value, CodecContext context) throws OperationNotSupportedException, EdgeDBException {
-        throw new OperationNotSupportedException("No data wire format has been defined for this type of codec");
+        if(value == null) {
+            writer.write(-1);
+            return;
+        }
+
+        if(!(value instanceof Collection<?>)) {
+            throw new IllegalArgumentException("The provided argument was not a collection");
+        }
+
+
+        var collection = ((Collection<?>)value).toArray();
+
+        writer.write(collection.length);
+
+        var visitor = context.getTypeVisitor();
+
+        for(int i = 0; i != collection.length; i++) {
+            writer.write(0); // reserved
+
+            var elementValue = collection[i];
+
+            if(elementValue == null) {
+                writer.write(-1);
+                continue;
+            }
+
+            visitor.setTargetType(elementValue.getClass());
+            Codec codec = visitor.visit(this.innerCodecs[i]);
+            visitor.reset();
+
+            writer.writeDelegateWithLength((v) -> codec.serialize(v, elementValue, context));
+        }
     }
 
     @Nullable
     @Override
     public Object deserialize(PacketReader reader, CodecContext context) throws EdgeDBException, OperationNotSupportedException {
-        throw new OperationNotSupportedException("No data wire format has been defined for this type of codec");
+        var numElements = reader.readInt32();
+
+        if(numElements != this.innerCodecs.length) {
+            throw new EdgeDBException("Expected " + this.innerCodecs.length + " elements, but got " + numElements);
+        }
+
+        var elements = new Object[numElements];
+
+        for(var i = 0; i != numElements; i++) {
+            reader.skip(INT_SIZE);
+
+            try(var elementReader = reader.scopedSlice()) {
+                elements[i] = innerCodecs[i].deserialize(elementReader, context);
+            }
+        }
+
+        return elements;
     }
 }
