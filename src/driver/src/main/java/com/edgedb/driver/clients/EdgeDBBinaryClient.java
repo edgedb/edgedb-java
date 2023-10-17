dfb97ddb8b9fb5f9d5000a308eadc395d9a311d0
Quin Lynch <lynchquin@gmail.com>
2023-10-17 20:49:55 -0300

    remove duplicate reconnect behavior
    Binding would call `reconnect()` which then gets reconnected on duplexer level just to sent Terminate and hang :cry:

--- src/driver/src/main/java/com/edgedb/driver/clients/EdgeDBBinaryClient.java
+++ src/driver/src/main/java/com/edgedb/driver/clients/EdgeDBBinaryClient.java
@@ -299,12 +299,7 @@ public abstract class EdgeDBBinaryClient extends BaseEdgeDBClient {
             @NotNull ExecutionArguments args
     ) {
         logger.debug("Execute request: is connected? {}", getDuplexer().isConnected());
-        if(!getDuplexer().isConnected()) {
-            // TODO: check for recursion
-            return reconnect()
-                    .thenCompose(v -> executeQuery(args));
-        }
-
+        
         final var hasReleased = new AtomicBoolean();
 
         return CompletableFuture.runAsync(() -> {
